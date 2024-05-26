2861ad1beb6444940dba3664bb6b2d1337c3943e
Morid Aghili <morid.aghili_92@yahoo.com>
2024-05-26 14:06:49 -0700

    x1

--- HoverBoardMindMotion/Inc/bldc.h
+++ HoverBoardMindMotion/Inc/bldc.h
@@ -1,7 +1,7 @@
 #ifdef TARGET_MM32SPIN25
 #include "HAL_device.h"                 // Device header
 #else
-#include "mm32_device.h"                // Device header
+#include "mm32_device.h"                // Device headerok
 #endif
 
 void commutate();
